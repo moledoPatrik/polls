e066ec05adb85e947aa5ae908446e2d2a543c7c8
Patrik Moledo <moledop55@gmail.com>
2023-02-10 22:18:20 +0900

    bootstrap and simpleform setup, poll and option models/controllers

---
+++ app/controllers/polls_options_controller.rb
@@ -0,0 +1,2 @@
+class PollsOptionsController < ApplicationController
+end
---
+++ spec/factories/poll_options.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :poll_option do
+    description { "MyString" }
+    poll { nil }
+  end
+end
